f230148e6602fd9d9d5ba8af5d658f265370142f
Precious <preciousmaregwa1997@gmail.com>
2022-04-08 14:13:53 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function(activity2 = "go to the office"){
+    return `This Monday, I will ${activity2}.`;
+  }
+  
+  function wrapAdjective(visualFlair = "*"){
+    return function (text = "special"){
+      return `You are ${visualFlair}${text}${visualFlair}!`;
+    }
+  }
